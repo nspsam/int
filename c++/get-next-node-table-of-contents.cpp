acfe0a6f8a222a3b4d23a4537428e9198100a895
nspsam <nsivaramprasad@gmail.com>
2018-03-27 01:42:11 -0400

    Formatted

--- c++/get-next-node-table-of-contents.cpp
+++ c++/get-next-node-table-of-contents.cpp
@@ -1,15 +1,22 @@
 /** 
 
-Table of Contents
+Table of Contents / Chapter tree
 1
 1.1
 1.1.1
 1.1.2
 1.2
 1.2.1
-1.2.1.1
 2
 3
+
+(or)
+
+            1
+        /   |   \
+     1.1    1.2  1.3
+     / \      \
+1.1.1  1.1.2  1.2.1   
 
 Given: node.getParent(), node.getNextSibling(), node.getFirstChild()
 Implement: getNextNode(node) - Print next bullet in printed order
@@ -29,7 +36,7 @@ class Solution {
     if(node->getFirstChild())
       return node->getFirstChild();
 	  
-    while(!node->getNextSibling() && node->getParent())
+    while(nullptr == node->getNextSibling() && node->getParent())
       node = node->getParent();
 	  
     return  node->getNextSibling();
